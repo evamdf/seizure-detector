69492e8cb93a0c9d75db5caea2252aedd3644a1c
eva-dustonfursman <eva.dustonfursman@starboard.nz>
2026-02-24 21:12:02 +1300

    Clean classifier & features

--- src/classifier.py
+++ src/classifier.py
@@ -5,24 +5,21 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 import pickle
 
-from shared.features import extract_features
-from shared.loader import load_dataset
-from shared.windows import segment_windows
+from features import extract_features
+from windows import segment_windows
 
-WINDOW_SIZE = 173   # ~1 second at 173.61 Hz
-STEP_SIZE   = 87    # 50% overlap
-
+from variables import MODEL_PATH
 
 def window_features(segments):
     """Extract features from every window across a list of segments."""
     X, y = [], []
     for segment in segments:
-        for window, label in segment_windows(segment, WINDOW_SIZE, STEP_SIZE):
+        for window, label in segment_windows(segment):
             X.append(extract_features(window))
             y.append(label)
     return np.array(X), np.array(y)
 
-def train(train_segs, model_path="model.pkl"):
+def train(train_segs):
     
     print("Extracting features...")
     X_train, y_train = window_features(train_segs)
@@ -34,17 +31,12 @@ def train(train_segs, model_path="model.pkl"):
 
     pipeline.fit(X_train, y_train)
 
-    with open(model_path, "wb") as f:
+    with open(MODEL_PATH, "wb") as f:
         pickle.dump(pipeline, f)
-    print(f"Model saved to {model_path}")
+    print(f"Model saved to {MODEL_PATH}")
     return pipeline
 
 
-def load_model(model_path="model.pkl"):
-    with open(model_path, "rb") as f:
+def load_model():
+    with open(MODEL_PATH, "rb") as f:
         return pickle.load(f)
-
-
-if __name__ == "__main__":
-    dataset = load_dataset()
-    train(dataset)
---
+++ src/variables.py
@@ -0,0 +1,33 @@
+SIMULATED_SPEED = 10.0          # How many times faster than real-time to simulate
+
+WINDOW_SIZE = 173   # ~1 second at 173.61 Hz
+STEP_SIZE   = 87    # 50% overlap 
+
+SAMPLING_RATE = 173.61 # Hz
+
+DATA_DIR = "../data/raw/"
+
+MODEL_PATH = "model.pkl"
+
+ALERT_THRESHOLD = 6 # Alert seizure after this many consecutive windows predicted as seizure (6 is 3 seconds)
+
+TEST_SIZE = 0.2  # proportion of segments to use as test set
+
+SET_LABELS = {
+    "F": 0,  # healthy, eyes open
+    "N": 0,  # healthy, eyes closed
+    "O": 0,  # interictal (between seizures)
+    "S": 1,  # interictal (between seizures)
+    "Z": 0,  # ictal (seizure)
+}
+
+BANDS = {
+    "delta": (0.5, 4),
+    "theta": (4, 8),
+    "alpha": (8, 13),
+    "beta":  (13, 30),
+    "gamma": (30, 60),
+}
+
+
+
